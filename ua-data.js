a2827cb802e664b09776649108e6157289fad2ed
datinghub26 <mamnunahmedcpa@gmail.com>
2026-06-23 22:19:21 -0700

    Create ua-data.js

---
+++ ua-data.js
@@ -0,0 +1,23 @@
+// ua-data.js - Device data for User Agent Generator
+const androidDevices = [
+  "SM-G998B", "SM-S928B", "SM-S918B", "SM-S931B", "SM-A715F", 
+  "SM-G996U", "SM-S146VL", "SM-F956N", "Pixel 8", "Pixel 9 Pro",
+  "vivo 1716", "vivo 1907", "vivo 2254", "CPH2581", "CPH2629", 
+  "CPH2363", "TECNO KJ5", "Infinix X6532", "Infinix X6851B",
+  "2506BPN68G", "T450H", "SM-S906W", "SM-S931W", "RMX3430"
+];
+
+const chromeVersions = [
+  "149.0.7827.116",
+  "148.0.7778.167",
+  "147.0.7727.24",
+  "149.0.7827.150",
+  "148.0.7778.215",
+  "148.0.7778.178"
+];
+
+const iosVersions = [
+  "26.5.1", "26.5", "26.4.1", "26.4", "26.3", "26.2", "26.1", "26.0"
+];
+
+export { androidDevices, chromeVersions, iosVersions };
